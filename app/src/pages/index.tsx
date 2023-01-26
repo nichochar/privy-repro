7822390c23c8bef599af4c0946efe48f746ad007
Nicholas Charriere <nicholascharriere@gmail.com>
2023-01-26 13:42:37 -0800

    Adding style-components breaks things. I suspect next

--- app/src/pages/index.tsx
+++ app/src/pages/index.tsx
@@ -2,6 +2,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from '@next/font/google'
 import styles from '@/styles/Home.module.css'
+import {StyledDivThing} from 'lib2';
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -38,6 +39,7 @@ export default function Home() {
             </a>
           </div>
         </div>
+	<StyledDivThing>Hello</StyledDivThing>
 
         <div className={styles.center}>
           <Image
