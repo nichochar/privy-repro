1a4e149432fd6e51584a6388f87f58ba81a144ea
Josh Acheson <joshua.acheson@gmail.com>
2023-01-25 20:11:34 -0500

    remove dist folders

--- lib1/dist/index.js
+++
@@ -1,1 +0,0 @@
-import{thing as o,forward as r}from"lib2";import{erc20ABI as t}from"wagmi";console.log(o,r);var i="another thing to export",g=t;export{g as forward2,i as thing2};
--- lib2/dist/index.js
+++
@@ -1,1 +0,0 @@
-import{erc20ABI as o}from"wagmi";var r="thing to log",n=o;export{n as forward,r as thing};
